1c77caae50b7b11bf879270077e38d3426da0866
HgSpartan98 <blazikenfreek@gmail.com>
2018-05-05 23:58:55 -0400

    Edits to infile

--- questionsFinalProject.py
+++ questionsFinalProject.py
@@ -1,11 +1,20 @@
 # Final Project
 # Aeowyn Kendall and Daniel Kim
 
-# We need to build a program which will give the user a choice between two quizzes then
-# call questions from a txt file, ask for user input, and check the input for certain
-# keywords, and output different text if the keywords match
+# We need to build a program which will give the user a choice between two quizzes
+# then call questions from a txt file, ask for user input, and check the input
+# for certain keywords, and output different text if the keywords match
 
 def question(qNumber, qSet):
-    qSet = 
+    if readQ != True:
+        qSet += ".txt"
+        infile = open(qSet, "r")
 
-    infile = open(spockQuestions.txt
+        textQ = infile.readline()
+
+        infile.closed
+
+        global read
+        readQ = True
+
+
