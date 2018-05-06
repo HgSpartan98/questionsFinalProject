e5f3a40b94b4ad90f3dae95a1499fe55c9c5e793
HgSpartan98 <blazikenfreek@gmail.com>
2018-05-05 23:06:03 -0400

    Main File
    Initial Main file. has the beginning of  code to fetch questions

---
+++ questionsFinalProject.py
@@ -0,0 +1,11 @@
+# Final Project
+# Aeowyn Kendall and Daniel Kim
+
+# We need to build a program which will give the user a choice between two quizzes then
+# call questions from a txt file, ask for user input, and check the input for certain
+# keywords, and output different text if the keywords match
+
+def question(qNumber, qSet):
+    qSet = 
+
+    infile = open(spockQuestions.txt
