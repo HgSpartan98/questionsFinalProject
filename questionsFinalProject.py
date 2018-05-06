7c8c4339fc975e3234177b015b5e0f20d6fdb47b
AeowynKendall <ak1395@messiah.edu>
2018-05-06 00:02:34 -0400

    added printIntro() and corrected length of lines

--- questionsFinalProject.py
+++ questionsFinalProject.py
@@ -1,10 +1,19 @@
 # Final Project
 # Aeowyn Kendall and Daniel Kim
 
-# We need to build a program which will give the user a choice between two quizzes then
-# call questions from a txt file, ask for user input, and check the input for certain
-# keywords, and output different text if the keywords match
+# We need to build a program which will give the user a choice between
+# two quizzes then call questions from a txt file, ask for user input, and
+# check the input for certain keywords, and output different text if the
+# keywords match
 
+def printIntro():
+
+    print("The following examination is designed to test the memory of a",
+          "previously incapacitated individual. If you answer a question",
+          "incorrectly, you will not be able to go back, but must proceed",
+          "to the next question.")
+    print()
+    
 def question(qNumber, qSet):
     qSet = 
 
