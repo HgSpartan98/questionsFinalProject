1b4e327144621fd3c6b8b2e4d61e5a9c19de799c
AeowynKendall <ak1395@messiah.edu>
2018-05-08 09:17:59 -0400

    Created a choose test program

---
+++ chooseTest.py
@@ -0,0 +1,13 @@
+# Choice
+
+def chooseTest():
+
+    testVersion = input('''Would you like to take the Star Trek memory test [S]
+    or the real life version [R]? ''').lower()
+
+    while testVersion == "s":
+        # pull qs and as from star trek files 
+        if testVersion == "r":
+        # pull info from real life files
+                        
+chooseTest()
