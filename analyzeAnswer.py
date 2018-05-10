73f3eb839e9ff8a5a087869cabff1c9722db98ea
AeowynKendall <ak1395@messiah.edu>
2018-05-10 01:48:17 -0400

    new code!!!

--- analyzeAnswer.py
+++ analyzeAnswer.py
@@ -3,7 +3,28 @@ def analyzeAnswer(dictionaryA, answerGiven, numberQ):
 
     answerReal = dictionaryA[numberQ]
     
-    if str(answerReal) in str(answerGiven):
-        return "CORRECT"
-    else:
-        return "INCORRECT"
+
+    
+    answers = dictionaryA[numberQ].split("$")
+    if len(answers) == 1:
+        if str(answerReal) in str(answerGiven):
+            return True
+        else:
+            return False
+        
+    elif len(answers) == 2:
+        answerReal = answers[0] or answers[1]
+        if str(answerReal) in str(answerGiven):
+            return True
+        else:
+            return False
+                
+    elif len(answers) == 3:
+        answerReal = answers[0] or answers[1] or answers[2]
+        if str(answerReal) in str(answerGiven):
+            return True
+        else:
+            return False
+        
+            
+        
